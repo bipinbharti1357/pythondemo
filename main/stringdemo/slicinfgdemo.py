10d15b7963fec3bfcf6ff868e0a5411c5d7ed4e2
bipinbharti1357 <dxgroup13571357@gmail.com>
2025-12-25 09:32:17 +0530

    ModifyString ongoing

--- main/stringdemo/slicinfgdemo.py
+++ main/stringdemo/slicinfgdemo.py
@@ -3,8 +3,31 @@
 
 # Specify the start index and the end index, separated by a colon, to return a part of the string.
 
+# Get the characters from position 2 to position 5 (not included):
+
 b = "Hello, World!"
 print(b[2:5])
+
+print("By leaving out the start index, the range will start at the first character:")
+print("Get the characters from the start to position 5 (not included):")
+print(b[:5])
+print("=========")
+print("Slice To the End")
+print("By leaving out the end index, the range will go to the end:")
+print("Get the characters from position 2, and all the way to the end:")
+print(b[2:])
+print("================================")
+print("Negative Indexing")
+print("Use negative indexes to start the slice from the end of the string:")
+#Example
+#Get the characters:
+
+#From: "o" in "World!" (position -5)
+
+#To, but not included: "d" in "World!" (position -2):
+print(b[-5:-2])
+
+
 
 
 
