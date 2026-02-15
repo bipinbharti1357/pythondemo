1898578b90c1b9adc816f94a47ef2cc2fd7bcdb0
bipinbharti1357 <dxgroup13571357@gmail.com>
2026-02-15 14:02:02 +0530

    tupels ongoing

---
+++ main/arrays/tupels.py
@@ -0,0 +1,8 @@
+marks = [85, 90, 78, 92, 88]
+print("Original marks:", marks)
+print("Type of marks:", type(marks))
+print("Length of marks:", len(marks))
+print("First mark:", marks[0])
+print("Last mark:", marks[-1])
+print("Marks from index 1 to 3:", marks[1:4])
+       
---
+++ main/stringdemo/modifystringdemo.py
@@ -0,0 +1,9 @@
+a = "Hello, World!"
+print(a.upper())
+
+#The upper() method returns the string in upper case:
+# The lower() method returns the string in lower case:
+# Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
+# The strip() method removes any whitespace from the beginning or the end:
+# The replace() method replaces a string with another string:
+
